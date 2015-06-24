6336dc654d71c83e2babbca906b1f9aa2844c3e8
Stefan Heimberg <stefan.heimberg@postfinance.ch>
2015-06-24 14:30:24 +0200

    initiales projektsetup

---
+++ src/test/java/ch/stefanheimberg/examples/jaxb_dateutc/DateUTCTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2015 Stefan Heimberg <kontakt@stefanheimberg.ch>.
+ * 
+ * All rights reserved.
+ */
+package ch.stefanheimberg.examples.jaxb_dateutc;
+
+/**
+ *
+ * @author Stefan Heimberg <kontakt@stefanheimberg.ch>
+ */
+public class DateUTCTest {
+    
+}
